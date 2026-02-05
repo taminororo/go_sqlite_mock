3f131dbcc6fb0f07136c6ee93bf0b879d0782454
taminororo <169162271+taminororo@users.noreply.github.com>
2026-02-06 02:09:47 +0900

    Learning of ExecContext

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func main() {
+	ctx := context.Background()
+
+	db, err := sql.Open("sqlite", "example.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// SQL命令には投げっぱなしのCRUD命令、結果がみたい検索命令がある
+	db.ExecContext(ctx, `CREATE TABLE IF NOT EXISTS balances (user_id INTEGER, balance INTEGER);`)
+	db.ExecContext(ctx, `INSERT INTO balances (user_id, balance) VALUES (41, 100);`)
+	// func (c *Conn) ExecContext(ctx context.Context, query string, args ...any) (Result, error)
+
+	// dbとのコネクションを確保
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	id := 41
+	result, err := conn.ExecContext(ctx, `UPDATE balances SET balance = balance + 10 WHERE user_id = ?;`, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("成功! 更新された行数：%d\n", rows)
+
+}
